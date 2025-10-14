2822248339ac6d60f7ae2c5334b5777bdfa586f4
Piotr Biernat <piotr1biernat@gmail.com>
2025-10-14 20:42:44 +0200

    Added bootstrap classes to a form fields

--- app/src/Form/PlayerType.php
+++ app/src/Form/PlayerType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Player;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,18 @@ class PlayerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('first_name')
-            ->add('last_name')
-            ->add('ranking')
-            ->add('active')
+            ->add('first_name', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('last_name', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('ranking', NumberType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('active', CheckboxType::class, [
+                'attr' => ['class' => 'form-check-input']
+            ])
         ;
     }
 
