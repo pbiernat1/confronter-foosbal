c2b122b485416cdf1d6c6eb816a17b8be07bb48d
Piotr Biernat <piotr1biernat@gmail.com>
2025-10-15 09:26:08 +0200

    Added stub match generator service with corresponding interface

---
+++ app/src/Infra/Interface/PlayerRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infra\Interface;
+
+use App\Entity\Player;
+
+interface PlayerRepositoryInterface
+{
+    public function findAllPlayers(): array;
+
+    public function findActivePlayers(): array;
+
+    public function findInactivePlayers(): array;
+
+    public function findPlayer(int $id): Player;
+}
