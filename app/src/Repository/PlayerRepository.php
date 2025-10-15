26f5669f9c0c5c38c08fa8c1c3fab5a724d5fe6d
Piotr Biernat <piotr1biernat@gmail.com>
2025-10-15 09:27:55 +0200

    Added base method in PlayerRepository that corresponding to the interface

--- app/src/Repository/PlayerRepository.php
+++ app/src/Repository/PlayerRepository.php
@@ -1,43 +1,61 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Repository;
 
 use App\Entity\Player;
+use App\Infra\Interface\PlayerRepositoryInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Player>
+ * @implements PlayerRepositoryInterface
  */
-class PlayerRepository extends ServiceEntityRepository
+class PlayerRepository extends ServiceEntityRepository implements PlayerRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Player::class);
     }
 
-//    /**
-//     * @return Player[] Returns an array of Player objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * Find all players
+     * @return Player[]
+     */
+    public function findAllPlayers(): array
+    {
 
-//    public function findOneBySomeField($value): ?Player
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return $this->findAll();
+    }
+
+    /**
+     * Find all active players
+     * @return Player[]
+     */
+    public function findActivePlayers(): array
+    {
+
+        return $this->findBy(['active' => true]);
+    }
+
+    /**
+     * Find all inactive players
+     * @return Player[]
+     */
+    public function findInactivePlayers(): array
+    {
+
+        return $this->findBy(['active' => false]);
+    }
+
+    /**
+     * Find player by id
+     * @param int $id
+     * @return Player
+     */
+    public function findPlayer(int $id): Player
+    {
+        return $this->find($id);
+    }
 }
